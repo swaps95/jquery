89deb11eca4da568c063aeaf13ad0b65ca78984f
unknown <Swapnil Chowkekar@GM-94.gomarketin.local>
2018-12-12 12:09:02 +0530

    update

--- sd.js
+++ sd.js
@@ -6,22 +6,53 @@ $('document').ready(function(){
 	
 	
 	$('.sliderrunner').css("width",(slidelen*100)+'%')
-//	$( "#Right" ).click(function() {
-//				$('.sliderrunner').animate({left:"-=100%"})
-//  });
+	
+
+	
+	
 	function runnslide(){
 		
 		if(count<(slidelen-1)){
-				$('.sliderrunner').animate({left:"-=100%"},500)
+				$('.sliderrunner').stop().animate({left:'-'+((count+1)*100)+"%"},500)
 			count=count+1;
 	 }
 	
 	else{
-			$('.sliderrunner').animate({left:"0%"},1500)
+			$('.sliderrunner').stop().animate({left:"0%"},1500)
 	count=0
 }
+		}	
+	
+	
+	
+	runslidevar=setInterval(runnslide,2000)
+
+			$( "#Right" ).click(function() {
+		clearInterval(runslidevar);		
+				runnslide();
+runslidevar=setInterval(runnslide,2000);
+     });		
 		
-	}
-runslidevar=setInterval(runnslide,2000)
+		
+		
+		
+			$( "#left" ).click(function() {
+		clearInterval(runslidevar);	
+				if(count>0){
+	$('.sliderrunner').stop().animate({left:"-"+((count-1)*100)+"%"},500)
+					count=count-1;
+}
+				else{
+							count=slidelen-1;
+									
+
+$('.sliderrunner').stop().animate({left:'-'+((count)*100)+"%"},1200)
+
+				}
+runslidevar=setInterval(runnslide,2000);
+     });		
+		
+		
+
 	
 })
